17298387959dab377c7cccacd0e5359efeb86a47
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-07 22:17:02 +0300

    Auth and User - controller/service layers starter

---
+++ src/main/java/com/capellax/grocery_app_backend/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.capellax.grocery_app_backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    // TODO
+
+}
