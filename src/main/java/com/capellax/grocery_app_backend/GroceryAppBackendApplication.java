291c89f1eae9848d2c25cef2df2ee821b66a6934
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-07 13:52:59 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/capellax/grocery_app_backend/GroceryAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capellax.grocery_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryAppBackendApplication.class, args);
+	}
+
+}
