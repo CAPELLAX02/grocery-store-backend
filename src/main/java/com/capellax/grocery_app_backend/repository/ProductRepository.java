9a98fe127588dedbc3b01601c02034e8095b4a92
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-24 12:19:02 +0300

    Fix review username update logic after user profile changes. Closes #1

--- src/main/java/com/capellax/grocery_app_backend/repository/ProductRepository.java
+++ src/main/java/com/capellax/grocery_app_backend/repository/ProductRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProductRepository
-        extends MongoRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 
     Page<Product> findAll(Pageable pageable);
+    List<Product> findAllByReviews_Username(String username);
 
 }
--- src/main/java/com/capellax/grocery_app_backend/service/user/UserService.java
+++ src/main/java/com/capellax/grocery_app_backend/service/user/UserService.java
@@ -3,12 +3,16 @@ package com.capellax.grocery_app_backend.service.user;
 import com.capellax.grocery_app_backend.dto.request.user.UpdateUserProfileRequest;
 import com.capellax.grocery_app_backend.dto.response.user.GetUserProfileResponse;
 import com.capellax.grocery_app_backend.dto.response.user.UpdateUserProfileResponse;
+import com.capellax.grocery_app_backend.model.product.Product;
 import com.capellax.grocery_app_backend.model.user.User;
+import com.capellax.grocery_app_backend.repository.ProductRepository;
 import com.capellax.grocery_app_backend.repository.UserRepository;
 import com.capellax.grocery_app_backend.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserServiceUtils userServiceUtils;
+    private final ProductRepository productRepository;
 
     public ApiResponse<GetUserProfileResponse> getUserProfile(
             String username
@@ -27,18 +32,33 @@ public class UserService {
     }
 
     public ApiResponse<UpdateUserProfileResponse> updateUserProfile(
-            String username,
+            String currentUsername,
             UpdateUserProfileRequest request
     ) {
-        User user = userServiceUtils.getUserByUsername(username);
+        User user = userServiceUtils.getUserByUsername(currentUsername);
 
-        user.setUsername(request.getUsername());
-//        user.setEmail(request.getEmail());
+        String oldUsername = user.getUsername();
+        String newUsername = request.getUsername();
+
+        if (!oldUsername.equals(newUsername)) {
+            user.setUsername(newUsername);
+
+            List<Product> products = productRepository.findAllByReviews_Username(oldUsername);
+            for (Product product : products) {
+                product.getReviews().forEach(review -> {
+                    if (review.getUsername().equals(oldUsername)) {
+                        review.setUsername(newUsername);
+                    }
+                });
+                productRepository.save(product);
+            }
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(user);
+
         // TODO: user email update will be distinct logic.
         //  A process like email activation process will be held.
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        userRepository.save(user);
 
         UpdateUserProfileResponse response = new UpdateUserProfileResponse();
         response.setMessage("User profile updated successfully.");
